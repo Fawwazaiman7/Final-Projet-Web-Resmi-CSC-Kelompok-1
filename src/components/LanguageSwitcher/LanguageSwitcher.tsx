cd5c0f0f249bb75e98c5c8381e1ea6b064f3ca96
Muhammad Fawwaz Aiman Muktia <fawwaz.aimanov@gmail.com>
2024-07-29 13:25:23 +0700

    modifikasi navbar untuk languageswitcher dan dark -mode

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,16 +1,46 @@
-import { useTranslation } from 'next-i18next';
+import { useTranslation } from "react-i18next";
+import Image from "next/image";
+import { useState, useEffect } from 'react';
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
   };
 
+  if (!mounted) return null;
+
   return (
-    <div>
-      <button onClick={() => changeLanguage('en')}>English</button>
-      <button onClick={() => changeLanguage('id')}>Bahasa Indonesia</button>
+    <div className="language-switcher">
+      <button className="dropbtn">
+        {i18n.language === "en" ? "English" : "Bahasa Indonesia"}
+      </button>
+      <div className="dropdown-content">
+        <a href="#" onClick={() => changeLanguage("en")}>
+          <Image
+            src="/icons8-great-britain-48.png"
+            alt="English"
+            width={20}
+            height={12}
+          />{" "}
+          English
+        </a>
+        <a href="#" onClick={() => changeLanguage("id")}>
+          <Image
+            src="/icons8-indonesia-48.png"
+            alt="Bahasa Indonesia"
+            width={20}
+            height={12}
+          />{" "}
+          Bahasa Indonesia
+        </a>
+      </div>
     </div>
   );
 };
