8d7fdd5f9e63f0747c748ea34d5e40522a4af4d7
Muhammad Fawwaz Aiman Muktia <fawwaz.aimanov@gmail.com>
2024-08-08 23:33:00 +0700

    menambahkan konten (3 section) pada halaman beranda (sisa aktifitas sama diviisi)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Lobster */}
         <link
           href="https://fonts.googleapis.com/css2?family=Lobster&family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        {/* Inter */}
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
           rel="stylesheet"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react';
-import { useTranslation } from 'next-i18next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { GetStaticProps } from 'next';
-import styles from "../styles/Banner/Banner.module.css";
+import Image from 'next/image';
+import styles from "../styles/Beranda/Beranda.module.css";
 
 const Home = () => {
-  const { t, i18n } = useTranslation('common');
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -14,16 +12,53 @@ const Home = () => {
 
   if (!isMounted) return null;
 
-  console.log('Translation function ready:', !!t);
-  console.log('Current language:', i18n.language);
-  console.log('i18n instance:', i18n);
-  console.log('Available translations:', i18n.getResourceBundle(i18n.language, 'common'));
-
   return (
-    <div className={`${styles.bannerBg} dark-banner`}>
-      <div className={styles.container}>
-        <h1 className={styles.headingBanner}>{t('welcome')}</h1>
+    <div className={styles.pageContainer}>
+      <div className={`${styles.bannerBg} dark-banner`}>
+        <div className={styles.container}>
+          <h1 className={styles.headingBanner}>Selamat datang di kelompok studi kami</h1>
+        </div>
       </div>
+      <section className={styles.contentSection}>
+        <div className={styles.contentItem}>
+          <div className={styles.imageContainer}>
+            <Image src="/icons/info.png" alt="Tentang CSC" width={200} height={200} />
+          </div>
+          <div className={styles.textContainer}>
+            <h2>Tentang CSC</h2>
+            <p>Computer Student Club (CSC) adalah kelompok studi mahasiswa di Politeknik Negeri Jakarta yang berfokus pada ranah keamanan siber, pengembangan perangkat lunak, dan IoT. Kami bertujuan untuk mengembangkan minat dan bakat mahasiswa serta meningkatkan prestasi mereka dalam bidang teknologi.</p>
+            <div className={styles.buttonContainer}>
+              <a href="/about" className={styles.button}>Ketahui lebih lanjut </a>
+            </div>
+          </div>
+        </div>
+        <div className={styles.spacing}></div>
+        <div className={styles.contentItem}>
+          <div className={styles.imageContainer}>
+            <Image src="/icons/question.png" alt="Ngapain aja sih di CSC?" width={200} height={200} />
+          </div>
+          <div className={styles.textContainer}>
+            <h2>Ngapain aja sih di CSC?</h2>
+            <p>Di Computer Student Club (CSC), kami aktif membimbing mahasiswa setiap pekan melalui kelas interaktif dan praktek secara langsung yang membahas berbagai aspek teknologi. Kelas yang kami adakan bertujuan untuk membantu memperluas pemahaman mahasiswa tentang keamanan siber, pengembangan perangkat lunak, dan perangkat IoT.</p>
+            <div className={styles.buttonContainer}>
+              <a href="/faq" className={styles.button}>Halaman FAQ </a>
+            </div>
+          </div>
+        </div>
+        <div className={styles.spacing}></div>
+        <div className={styles.contentItem}>
+          <div className={styles.imageContainer}>
+            <Image src="/icons/phone.png" alt="Boleh tanya-tanya dulu?" width={200} height={200} />
+          </div>
+          <div className={styles.textContainer}>
+            <h2>Boleh tanya-tanya dulu?</h2>
+            <p>Boleh banget! Kami selalu siap untuk menjawab pertanyaan dari anggota, non-anggota, atau siapa pun yang tertarik. Anda dapat menghubungi kami melalui Whatsapp atau Instagram kami. Kami berkomitmen untuk memberikan jawaban informatif dan membantu menjawab apapun pertanyaan yang anda miliki. Jangan ragu untuk bertanya atau berdiskusi dengan kami!</p>
+            <div className={styles.buttonContainer}>
+              <a href="/contact" className={styles.button}>Hubungi kami </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
@@ -31,7 +66,7 @@ const Home = () => {
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale as string, ['common'])),
+      // Static props
     },
   };
 };
